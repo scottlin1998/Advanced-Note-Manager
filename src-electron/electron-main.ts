cb6f4d3c8ddc0c65f7e4aed61f3aea1bb9378a60
“scottlin1998” <scottlin1998@outlook.com>
2021-09-11 10:42:16 +0800

    更新electron代码为typescript

--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -1,15 +1,16 @@
 import { app, BrowserWindow, nativeTheme } from 'electron'
-import path from 'path'
+import * as fs from 'fs'
+import * as path from 'path'
 
 try {
   if (process.platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
-    require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'))
+    fs.unlinkSync(path.join(app.getPath('userData'), 'DevTools Extensions'))
   }
 } catch (_) { }
 
-let mainWindow
+let mainWindow: null | BrowserWindow = null;
 
-function createWindow () {
+function createWindow() {
   /**
    * Initial window options
    */
@@ -20,11 +21,11 @@ function createWindow () {
     webPreferences: {
       contextIsolation: true,
       // More info: /quasar-cli/developing-electron-apps/electron-preload-script
-      preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD)
+      preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD as string)
     }
   })
 
-  mainWindow.loadURL(process.env.APP_URL)
+  mainWindow.loadURL(process.env.APP_URL as string)
 
   if (process.env.DEBUGGING) {
     // if on DEV or Production with debug enabled
@@ -32,7 +33,7 @@ function createWindow () {
   } else {
     // we're on production; no access to devtools pls
     mainWindow.webContents.on('devtools-opened', () => {
-      mainWindow.webContents.closeDevTools()
+      mainWindow?.webContents.closeDevTools()
     })
   }
 
